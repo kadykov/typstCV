1369bd8befd5eb6887f8706d3010ad1dd4bebe7a
Aleksandr Kadykov <git@kadykov.com>
2024-11-13 19:36:38 +0000

    Fix output-folder access in the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -l
 
 set -e
-just build
-mkdir -p ${INPUTS_OUTPUT_FOLDER}
-cp *.pdf *.html ${INPUTS_OUTPUT_FOLDER}/
-files=$(ls ${INPUTS_OUTPUT_FOLDER}/)
-echo -e "Files in the ${INPUTS_OUTPUT_FOLDER} folder:\n$files"
+
+# Use the correct environment variable for the input
+output_folder="${INPUT_OUTPUT_FOLDER}"
+
+# Ensure the output folder exists
+mkdir -p "$output_folder"
+
+# Copy generated files to the output folder
+cp *.pdf *.html "$output_folder/"
+
+# List files in the output folder and print them
+files=$(ls "$output_folder/")
+echo -e "Files in the $output_folder folder:\n$files"
